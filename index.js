1cf4f2787dc5dbac727dcd3f43e3372e001ea2ee
SpoltoreLucas <lucaspoltore2023@gmail.com>
2023-11-27 01:00:07 -0300

    desafio da dio-JScript conlcuido com sucesso

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-console.log("Olá mundo")
-console.log("Oi")
+const nomeHeroi = "Lucas"
+let xp = 99001
+let ranking
+
+if (xp <= 1000) {
+    ranking = "Ferro"
+} else if ((xp >= 1001) && (xp <= 2000)){ 
+    ranking = "Bronze"
+} else if ((xp >= 2001) && (xp <= 6000)){ 
+    ranking = "Prata"
+} else if ((xp >= 6001) && (xp <= 7000)){ 
+    ranking = "Ouro"
+} else if ((xp >= 7001) && (xp <= 8000)){ 
+    ranking = "Platina"
+} else if ((xp >= 8001) && (xp <= 9000)){ 
+    ranking = "Ascendente"
+} else if ((xp >= 9001) && (xp <= 10000)){ 
+    ranking = "Imortal"
+} else {ranking = "Radiante"}
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + ranking)
